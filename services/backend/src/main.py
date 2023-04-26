4bdc704405f4deb0aed7f385443614b4019391ce
ovrael <ovrael@gmail.com>
2023-04-26 23:16:53 +0200

    Working on image to text view Added file upload

--- services/backend/src/main.py
+++ services/backend/src/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI, Response, status
+from ast import List
+import os
+from fastapi import FastAPI, File, Response, UploadFile, status
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -21,3 +23,33 @@ def home():
 @app.get("/about")
 def about():
     return "This is about us page!"
+
+# @app.post("/uploadfiles/")
+# async def create_upload_files(files: List[UploadFile] = File(...)):
+#     result=""
+#     for file in files:
+#         print(file)
+#         # fpath = os.path.join(
+#         #     STORAGE_PATH, f'{file.filename}'
+#         # )
+#         # async with aiofiles.open(fpath, 'wb') as f:
+#         #     content = await file.read()
+#         #     await f.write(content)
+
+        
+
+#     return {"message": "success"}
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile | None = None):
+    if not file:
+        return {
+            "result": False,
+            "message": "No upload file sent.",
+            }
+    
+    return {
+        "result": True,
+        "message": "File received correctly.",
+        "filename": file.filename,
+        }
--- services/frontend/src/views/AiTools/ImageToTextView.vue
+++ services/frontend/src/views/AiTools/ImageToTextView.vue
@@ -1,15 +1,75 @@
 <template>
-    <UnderDeveloping site-name="Image to text"></UnderDeveloping>
+    <UnderDeveloping site-name=""></UnderDeveloping>
+    <div id="file-upload-list" class="container">
+        <div class="large-12 medium-12 small-12 cell">
+            <label>File
+                <input type="file" id="file" ref="file" v-on:change="handleFileUpload()" />
+            </label>
+            <button v-on:click="submitFile()">Submit</button>
+        </div>
+    </div>
 </template>
   
 <script>
 // @ is an alias to /src  
 import UnderDeveloping from '@/components/UnderDeveloping.vue';
+import axios from 'axios'
 
 export default {
     name: 'ImageToTextView',
     components: {
         UnderDeveloping
+    },
+    data() {
+        return {
+            file: ''
+        }
+    },
+    methods: {
+        /*
+            Submits the file to the server
+        */
+        submitFile() {
+            /*
+                    Initialize the form data
+                */
+            let formData = new FormData();
+
+            /*
+                Add the form data we need to submit
+            */
+            formData.append('file', this.file);
+
+            /*
+            Make the request to the POST /single-file URL
+            */
+            axios.post('/uploadfile', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            }).then(response => {
+                const data = response.data;
+
+                if (data.result == false) {
+                    console.warn(response.message);
+                    return;
+                }
+
+                console.log(data);
+
+
+            }).catch(error => {
+                console.error(error);
+            });
+
+        },
+
+        /*
+            Handles a change on the file upload
+        */
+        handleFileUpload() {
+            this.file = this.$refs.file.files[0];
+        }
     }
 }
 </script>
